7fc15f557ea71a76d85922ad04d36ee996d1b1de
mmaras02 <mmaras02@fesb.hr>
2024-11-29 01:01:54 +0100

    separating a function

--- src/pages/TypingTest.js
+++ src/pages/TypingTest.js
@@ -15,6 +15,8 @@ const TypingTest = () => {
     const [isCharCorrect, setIsCharCorrect] = useState(null);
 
     const [correctWrongChar, setCorrectWrongChar] = useState([]);
+    const [correctChar, setCorrecChar] = useState([]);
+    const [incorrectChar, setIncorrectChar] = useState([]);
     
     const inputRef = useRef(null);
     const charRefs = useRef([]);
@@ -32,54 +34,50 @@ const TypingTest = () => {
         focusInput();
     },[])
 
-    const handleUserInput = (e) => { //problem oznacavaju se sva prva slova svake rijeci sta znaci da nmg stavljat char na 0 za svaki rijec
+    const updateCharStatus = (wordIndex, charIndex, status) => {
+        setCorrectWrongChar((prev) => {
+          const newCorrectWrongChar = [...prev];
+          newCorrectWrongChar[wordIndex] = [...newCorrectWrongChar[wordIndex]];
+          newCorrectWrongChar[wordIndex][charIndex] = status;
+          return newCorrectWrongChar;
+        });
+    };
 
-        //const characters = charRefs.current; //access all the characters
-        //console.log("characters", characters);
+    const handleUserInput = (e) => {
+
         const input = e.target.value;
         setUserInput(input);
         console.log("input", input);
+        const typedWord = input.split(" ")[currentWordIndex] || "";
+        console.log("typed word", typedWord);
 
-        const activeWord = text[currentWordIndex]; //active word
-        //let activeChar = charRefs.current[currentCharIndex]; //active char
-        const activeChar = activeWord[currentCharIndex];
+        const currentWord = text[currentWordIndex];
+        const currentChar = currentWord[currentCharIndex];
 
-        console.log("active word", activeWord);
-        console.log("active char", activeChar);
-
-        //for space bar go current word is +1
         if(input.endsWith(" ")){
-            
-            setCurrentWordIndex((prevIndex) => prevIndex + 1);
-            setCurrentCharIndex(0);
-            //setCorrectWrongChar([]);
-            //setUserInput("");
-            console.log("wtf");
-            return;
-        }
-        //input usporedi s active rijeci 
+            console.log("activeWord.length", currentWord.length);
+            console.log("iinput length", typedWord.length);
 
-        if(input[input.length - 1] === activeChar){
+            if(currentWord.length >= typedWord.length){
+                setCurrentWordIndex((prevIndex) => prevIndex + 1); //iddemo na iducu rijec
+                setCurrentCharIndex(0); //u novoj rijeci prvo slovo je 0
+                return;
+            }else{
+                //setCurrentCharIndex((prevIndex) => prevIndex + 1);
+                updateCharStatus(currentWordIndex, currentCharIndex, "incorrect");
+            }
+            
+        }
+        if(input[input.length - 1] === currentChar){
             setCurrentCharIndex((prevIndex) => prevIndex + 1);
-            console.log("correct");
-            setCorrectWrongChar(prev => {
-                const newCorrectWrongChar = [...prev];
-                newCorrectWrongChar[currentWordIndex][currentCharIndex] = "correct";
-                return newCorrectWrongChar;
-            });
+            updateCharStatus(currentWordIndex, currentCharIndex, "correct");
         }else{
             setCurrentCharIndex((prevIndex) => prevIndex + 1);
-            console.log("wrong");
-            setCorrectWrongChar(prev => {
-                const newCorrectWrongChar = [...prev];
-                newCorrectWrongChar[currentWordIndex][currentCharIndex] = "wrong";
-                return newCorrectWrongChar;
-            });
-            
+            updateCharStatus(currentWordIndex, currentCharIndex, "incorrect");
         }
+        console.log("corect wrong char", correctWrongChar);
     }
-    const checkMatch = () => {
-    }
+
     return ( 
         <>
         
@@ -107,19 +105,36 @@ const TypingTest = () => {
                           ref={inputRef} 
                           onChange={handleUserInput}
                           value={userInput}
-                          autoFocus/>
+                          autoFocus
+                          spellcheck="false"/>
             </div>
         </div>
         </>
      );
+
 }
- 
 export default TypingTest;
 
 //moran vidit je li rijec aktivna ili ne
 //rijec koja je aktivna provjerit svako slovo s user input
 
 //mapirat kroz svaku rijec i onda kroz svako slovo pa provjeravat 
+
+//https://fonts.googleapis.com/css2?family=Azeret+Mono&display=swap
+
+//<p className={`char ${index === currentCharIndex && isCharCorrect ? 'correct' : 'mistake'}`}>{char}</p>
+
+
+//moran nap prvo slovo --> current 
+//ako je tocan char pridjelim mu class name 
+//situacije:
+//backspace--> delete letter prebious letter is currrent
+//space--> if input equals active word --> move to he next word 
+//         if user types space in incorrect place display as wrong and continue normally 
+//extra letters --> keep track of letters and num of spaces and words and depending on which char i am count which word it is and whictch char
+
+//ovisno koliko je vrimena rijeci se pojavljuju 
+//moran pratit koliko mistakes
 
 //https://fonts.googleapis.com/css2?family=Azeret+Mono&display=swap
 
