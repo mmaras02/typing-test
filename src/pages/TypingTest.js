281d8fb623c4db94e8b1a6be534c380ab58e74e5
mmaras02 <mmaras02@fesb.hr>
2024-11-30 02:44:58 +0100

    handling extra letters

--- src/pages/TypingTest.js
+++ src/pages/TypingTest.js
@@ -12,16 +12,16 @@ const TypingTest = () => {
     const [userInput, setUserInput] = useState("");
     const [currentWordIndex, setCurrentWordIndex] = useState(0);
     const [currentCharIndex, setCurrentCharIndex] = useState(0);
-    const [isCharCorrect, setIsCharCorrect] = useState(null);
 
     const [correctWrongChars, setCorrectWrongChars] = useState([]);
     const [correctChars, setCorrecChars] = useState(0);
     const [correctWords, setCorrectWords] = useState(0);
     const [incorrectChar, setIncorrectChar] = useState(0);
-    
+    const passedTime = testDuration / 60;
+
     const inputRef = useRef(null);
-    const wordRefs = useRef([]);
-    const charRefs = useRef([]);
+    //const wordRefs = useRef([]);
+    //const charRefs = useRef([]);
 
     const focusInput = () => {
         inputRef?.current.focus();
@@ -51,7 +51,6 @@ const TypingTest = () => {
 
         //space handling
         if(e.key === " " || e.key === "Enter"){
-            console.log("user input", userInput.trim());
             if(userInput.trim() === currentWord){
                 setCorrectWords(prev => prev + 1);
             }
@@ -62,12 +61,36 @@ const TypingTest = () => {
         }
 
         //backspace hendling
-        if(e.key === "Backspace"){
-            if(currentCharIndex > 0){
-                setCurrentCharIndex(prevIndex => prevIndex - 1); //vracamo se jedan unazad i tog unazad updateamo status
-                updateCharStatus(currentWordIndex, currentCharIndex - 1, null);
-                setUserInput(prev => prev.slice(0, -1));
+        if (e.key === "Backspace") {
+            if (currentCharIndex > 0) {
+                setCurrentCharIndex((prevIndex) => prevIndex - 1);
+                setUserInput((prev) => prev.slice(0, -1));
+
+                if (correctWrongChars[currentWordIndex][currentCharIndex - 1]  === "incorrect extra") {
+                    setText((prev) => {
+                        const newText = [...prev];
+                        newText[currentWordIndex] = newText[currentWordIndex].slice(0, -1);
+                        return newText;
+                    });
+                }else{
+                    updateCharStatus(currentWordIndex, currentCharIndex - 1, null);
+                }
             }
+            return;
+        }
+    
+        // Handling extra letters
+        if (currentCharIndex >= currentWord.length) {
+            
+            setText((prev) => {
+                const newText = [...prev];
+                newText[currentWordIndex] += e.key; 
+                return newText;
+            });
+            updateCharStatus(currentWordIndex, currentCharIndex, "incorrect extra");
+            setUserInput((prev) => prev + e.key); 
+            setIncorrectChar((prev) => prev + 1); 
+            setCurrentCharIndex((prevIndex) => prevIndex + 1);
             return;
         }
 
@@ -84,7 +107,6 @@ const TypingTest = () => {
 
     const calculateAccuracy = () => {
         const totalWordsTyped = userInput.split(" ").length;
-        const passedTime = testDuration / 60;
 
         const nwpm = correctWords / passedTime;
         const gwpm = totalWordsTyped / passedTime;
@@ -94,7 +116,6 @@ const TypingTest = () => {
     const calculateSpeed = () => {
         const allTypedChar = userInput.length;
         console.log("all typed ", allTypedChar);
-        const passedTime = testDuration / 60;
         return Math.round((allTypedChar / 5) / passedTime);
     }
 
@@ -102,31 +123,45 @@ const TypingTest = () => {
         <>
         
         <div className="test-container">
-            {/*<div className="header">
-                <h1>{testDuration} Typing Test</h1>
-            </div>*/}
-            <div className="text-box" onClick={focusInput}>
-                {loading ? (
-                    <p>Loading story...</p>
-                ) : (
-                    <div className="words">
-                        {text.map((word, wordIndex) => (
-                            <div className="word" key={wordIndex}>
-                                {word.split('').map((char, charIndex) => (
-                                    
-                                    <span key={`${wordIndex}-${charIndex}`} className={`char ${wordIndex === currentWordIndex && charIndex === currentCharIndex ? "active" : ""} ${correctWrongChars[wordIndex][charIndex]}`}>{char}</span>
-                                ))}
-                        </div>
-                    ))}
+            
+            <div className="center-box">
+                <div className="top-menu">
+                    <h2>{currentWordIndex} / 20</h2>
+                    <div className="mode-menu">
+                        <h4>Mode |</h4>
+                        <button className="option-button">Time</button>
+                        <button className="option-button active">Words</button>
                     </div>
-                )}
-                <textarea type="text"
-                          className="hidden-input"
-                          ref={inputRef} 
-                          value={userInput}
-                          onChange={(e) => setUserInput(e.target.value)}
-                          onKeyDown={onKeyDown}
-                          autoFocus/>
+                    <div className="options-menu">
+                        <button className="option-button active">20</button>
+                        <button className="option-button">30</button>
+                        <button className="option-button">50</button>
+                    </div>
+                </div>
+
+                <div className="text-box" onClick={focusInput}>
+                    {loading ? (
+                        <p>Loading story...</p>
+                    ) : (
+                        <div className="words">
+                            {text.map((word, wordIndex) => (
+                                <div className="word" key={wordIndex}>
+                                    {word.split('').map((char, charIndex) => (
+                                        
+                                        <span key={`${wordIndex}-${charIndex}`} className={`char ${wordIndex === currentWordIndex && charIndex === currentCharIndex ? "active" : ""} ${correctWrongChars[wordIndex][charIndex]}`}>{char}</span>
+                                    ))}
+                            </div>
+                        ))}
+                        </div>
+                    )}
+                    <textarea type="text"
+                            className="hidden-input"
+                            ref={inputRef} 
+                            value={userInput}
+                            onChange={(e) => setUserInput(e.target.value)}
+                            onKeyDown={onKeyDown}
+                            autoFocus/>
+                </div>
             </div>
         </div>
         </>
